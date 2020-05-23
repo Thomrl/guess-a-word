8bd5b0e7e067eb93ca771ddc1497d10ffb915a7a
Thomrl <tommer.rl@live.dk>
2020-05-24 01:18:38 +0200

    Added new Win/Lose popup

--- js/index-apps.js
+++ js/index-apps.js
@@ -9,10 +9,17 @@ wrong = 0;
 checkTheWord = "";
 var foundLetters = [];
 var userInput = " ";
+var words = ["keyboard", "bell", "space", "universe", "grass", "flower", "nuts", "hero", "wheel", "zoom", "camera", "video", "chat", "super", "water", "fire", "wind", "earth", "mars", "power", "paper", "pen", "awesome", "dice", "ball", "computer", "phone", "facebook", "twitter", "instagram", "car", "bank", "orange", "blue", "green", "yellow", "black", "cool", "happy", "sweet", "monkey", "dog", "cat", "mouse", "rat", "zoo", "ape", "glue", "man", "woman", "boy", "girl", "baby", "adult", "kid", "child", "drum", "guitar", "code", "christmas", "winter", "summer", "easter", "spring"];    
+var gamePlaying = true;
 
 
 var init = function() {
-    var words = ["keyboard", "bell", "space", "universe", "grass", "flower", "nut", "hero", "wheel", "zoom", "camera", "video", "chat", "super", "water", "fire", "wind", "earth", "mars", "power", "paper", "pen", "awesome", "dice", "ball", "computer", "phone", "facebook", "twitter", "instagram", "car", "bank", "orange", "blue", "green", "yellow", "black", "cool", "happy", "sweet", "monkey", "dog", "cat", "mouse", "rat", "zoo", "ape", "glue", "man", "woman", "boy", "girl", "baby", "adult", "kid", "child", "drum", "guitar", "code", "christmas", "winter", "summer", "easter", "spring"];
+    gamePlaying = true;
+    wrongLetters = [];
+    displayedWrongLetters = "";
+    document.getElementById("wrong-letters").textContent = displayedWrongLetters;
+    document.getElementById("hmimg").src = "img/1-hangman.jpg";
+    document.querySelector(".maps").style.display = "none";
     theWord = words[Math.floor(Math.random() * words.length)];
     for (i=0; i < theWord.length; i++) {
         aTheWord.push(theWord[i]);
@@ -37,9 +44,9 @@ function showResultAsLines() {
             aSResult.splice(i, 1, "_");
         }
     }
-
+    
     console.log("Show Result = " + aSResult)
-
+    
     showedResult = aSResult;
     showedResult = showedResult.join(" ");
     showedResult = showedResult.toUpperCase();
@@ -65,10 +72,7 @@ if (toggleHM === "true") {
 } else {
     localStorage.setItem("toggleHM", "false");
     toggleHM = localStorage.getItem('toggleHM');
-
 }
-
-
 
 
 //toggle on click
@@ -83,12 +87,33 @@ document.getElementById("toggle-hangman").addEventListener("click", function() {
         toggleHM = localStorage.getItem('toggleHM');
     } else {
         localStorage.setItem("toggleHM", "true");
-
     }
-
-
+    
+    
 })
 
+//Hangman section end..
+
+function newGame(){
+    var theWord;
+    aTheWord = [];
+    aSResult = [];
+    aShowedResult = [];
+    var showedResult = "";
+    var i;
+    maxWrong = 8;
+    wrong = 0;
+    checkTheWord = "";
+    var foundLetters = [];
+    var userInput = " ";
+    var gamePlaying = true;
+    init();
+};
+
+document.getElementById("new-game").addEventListener("click", function() {
+    newGame();
+    
+});
 
 
 
@@ -97,55 +122,66 @@ wrongLetters = [];
 
 
 function check() {
-    var userInput = document.getElementById("userInput").value.toString();
-    userInput = userInput.toLowerCase();
-    
-    
-    if (theWord.includes(userInput) === true) { //If the userinput matches a letter
-        for (i=0; i < aTheWord.length; i++) {
-            if (aTheWord[i].includes(userInput)) { //find that letter
-                aSResult.splice((i), 1, userInput);  //replace number with userinput
-                aShowedResult = aShowedResult.splice(i, 1, userInput);
+    if (gamePlaying === true) {
+        var userInput = document.getElementById("userInput").value.toString();
+        userInput = userInput.toLowerCase();
+        
+        
+        if (theWord.includes(userInput) === true) { //If the userinput matches a letter
+            for (i=0; i < aTheWord.length; i++) {
+                if (aTheWord[i].includes(userInput)) { //find that letter
+                    aSResult.splice((i), 1, userInput);  //replace number with userinput
+                    aShowedResult = aShowedResult.splice(i, 1, userInput);
+                }
             }
+        } else {
+            //Show how many mistakes made.
+            wrong = wrong+1;
+            document.getElementById("wrongind").textContent = wrong;
+            //Show the wrong letters.
+            wrongLetters.push(userInput, " ")
+            displayedWrongLetters = wrongLetters.join();
+            displayedWrongLetters = displayedWrongLetters.replace(/,/g, " ");
+            document.getElementById("wrong-letters").textContent = displayedWrongLetters;
+            //change image
+            document.getElementById("hmimg").src = "img/"+(wrong+1)+"-hangman.jpg";
+            
+            
         }
-    } else {
-        //Show how many mistakes made.
-        wrong = wrong+1;
-        document.getElementById("wrongind").textContent = wrong;
-        //Show the wrong letters.
-        wrongLetters.push(userInput, " ")
-        displayedWrongLetters = wrongLetters.join();
-        displayedWrongLetters = displayedWrongLetters.replace(/,/g, " ");
-        document.getElementById("wrong-letters").textContent = displayedWrongLetters;
-        //change image
-        document.getElementById("hmimg").src = "img/"+(wrong+1)+"-hangman.jpg";
-
+        
+        showResultAsLines();
+        document.getElementById("showed-result").textContent = showedResult//showedResult;
+        
+        if (wrong === maxWrong) {
+            //alert("Game Over!\nReload page the play again!");
+            document.getElementById("win-lose-text").textContent = "Game Over!"
+            document.querySelector(".maps").style.display = "block";
+            gamePlaying = false;
+        };
+        
+        checkThis = aSResult.join("");
+        
+        if (checkThis === theWord) {
+            //alert("Congratiulations, you won!\nReload page to play again!");
+            document.getElementById("win-lose-text").textContent = "Congratiulations! You Won!"
+            document.querySelector(".maps").style.display = "block";
+            gamePlaying = false;
+        }
+        
+        
+        console.log("Clicked!")
+        document.getElementById("userInput").text= "";
+        
         
     }
-
-    showResultAsLines();
-    document.getElementById("showed-result").textContent = showedResult//showedResult;
     
-    if (wrong === maxWrong) {
-        alert("Game Over!\nReload page the play again!");
-    };
-
-    checkThis = aSResult.join("");
-    
-    if (checkThis === theWord) {
-        alert("Congratiulations, you won!\nReload page to play again!");
-    }
-    
-    
-    console.log("Clicked!")
-    document.getElementById("userInput").text= "";
 };
 
 //////////////////////////
 
 //Planned
 
-//alternative to alerts
+//alternative to alerts //Done
 
 //categories
 
@@ -153,11 +189,16 @@ function check() {
 
 //////////////////////////
 
-
 //Enter
 window.addEventListener("keydown", function(e) {
     if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
-    check(e);
+    
+    if (gamePlaying === true) {
+        check(e);
+    } else {
+        newGame(e);
+    }
+    
 }
 });
 
